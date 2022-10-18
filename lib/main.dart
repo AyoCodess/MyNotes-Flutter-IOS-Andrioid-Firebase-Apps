0008786c1093678317e389a86e17d635ec5e7a51
AyoCodess <ayo.daniel.adesanya@gmail.com>
2022-10-18 08:52:59 +0200

    added menu and alert dialogue box

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:mynotes/firebase_options.dart';
 import 'package:mynotes/views/login_view.dart';
 import 'package:mynotes/views/register_view.dart';
 import 'package:mynotes/views/verifyemail_view.dart';
+import 'dart:developer' as devtools show log;
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,7 +13,7 @@ void main() {
     MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.red,
+        primarySwatch: Colors.green,
       ),
       home: const HomePage(),
       routes: {
@@ -35,23 +36,74 @@ class HomePage extends StatelessWidget {
       builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
         switch (snapshot.connectionState) {
           case ConnectionState.done:
-            // final user = FirebaseAuth.instance.currentUser;
-            // print(user);
-            // // FirebaseAuth.instance.signOut();
-            // if (user == null) {
-            //   return const RegisterView();
-            // }
-
-            // if (user.emailVerified) {
-            //   return const Text('User\'s email is verified');
-            // } else {
-            //   return const VerifyEmailView();
-            // }
-            return const LoginView();
+            final user = FirebaseAuth.instance.currentUser;
+            if (user != null) {
+              if (user.emailVerified) {
+                return const NotesView();
+              } else {
+                return const VerifyEmailView();
+              }
+            } else {
+              return const LoginView();
+            }
           default:
             return const CircularProgressIndicator();
         }
       },
     );
   }
+}
+
+enum MenuOptions { logout }
+
+class NotesView extends StatefulWidget {
+  const NotesView({super.key});
+
+  @override
+  State<NotesView> createState() => _NotesViewState();
+}
+
+class _NotesViewState extends State<NotesView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My Notes'),
+        actions: [
+          PopupMenuButton<MenuOptions>(
+            onSelected: (option) {
+              if (option == MenuOptions.logout) {
+                devtools.log('log out');
+                showLogOutDialog(context);
+              }
+            },
+            itemBuilder: (context) => [
+              const PopupMenuItem(
+                value: MenuOptions.logout,
+                child: Text('Logout'),
+              )
+            ],
+          )
+        ],
+      ),
+      body: const Text('Notes'),
+    );
+  }
+}
+
+Future<bool> showLogOutDialog(BuildContext context) async {
+  return await showDialog<bool>(
+      context: context,
+      builder: (context) => AlertDialog(
+            title: const Text('Logout'),
+            content: const Text('Are you sure you want to logout?'),
+            actions: [
+              TextButton(
+                  onPressed: () => Navigator.pop(context, false),
+                  child: const Text('Cancel')),
+              TextButton(
+                  onPressed: () => Navigator.pop(context, true),
+                  child: const Text('Logout'))
+            ],
+          )).then((value) => value ?? false);
 }
